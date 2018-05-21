30ad0495edd3985129239f8ac1e21c7c0738cb6e
Clint Lee <l.clint91@gmail.com>
2018-05-21 15:33:19 -0400

    first commit after building a new one from scratch

---
+++ dev/scripts/NewSynth.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+export default function(props) {
+    return (
+        <div>
+            <div>
+                <input type="checkbox" id="C5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C5" onChange={() => props.triggerNote(props.type)} />
+            </div>
+            <div>
+                <input type="checkbox" id="D5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="D5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="D5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="D5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="D5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="D5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="D5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="D5" onChange={() => props.triggerNote(props.type)} />
+            </div>
+            <div>
+                <input type="checkbox" id="E5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="E5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="E5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="E5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="E5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="E5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="E5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="E5" onChange={() => props.triggerNote(props.type)} />
+            </div>
+            <div>
+                <input type="checkbox" id="F5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="F5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="F5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="F5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="F5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="F5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="F5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="F5" onChange={() => props.triggerNote(props.type)} />
+            </div>
+            <div>
+                <input type="checkbox" id="G5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="G5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="G5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="G5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="G5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="G5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="G5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="G5" onChange={() => props.triggerNote(props.type)} />
+            </div>
+            <div>
+                <input type="checkbox" id="A5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="A5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="A5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="A5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="A5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="A5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="A5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="A5" onChange={() => props.triggerNote(props.type)} />
+            </div>
+            <div>
+                <input type="checkbox" id="B5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="B5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="B5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="B5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="B5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="B5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="B5" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="B5" onChange={() => props.triggerNote(props.type)} />
+            </div>
+            <div>
+                <input type="checkbox" id="C6" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C6" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C6" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C6" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C6" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C6" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C6" onChange={() => props.triggerNote(props.type)} />
+                <input type="checkbox" id="C6" onChange={() => props.triggerNote(props.type)} />
+            </div>
+        </div>
+    )
+}
--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,150 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NewSynth from './NewSynth';
 
 class App extends React.Component {
-    render() {
-      return (
-        <div>
-          Hello
-        </div>
-      )
+  constructor() {
+    super();
+    this.state = {
+      type: '',
+      types: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmitForSawtooth = this.handleSubmitForSawtooth.bind(this);
+    this.handleSubmitForSine = this.handleSubmitForSine.bind(this);
+    this.handleSubmitForSquare = this.handleSubmitForSquare.bind(this);
+    this.handleSubmitForTriangle = this.handleSubmitForTriangle.bind(this);
+    this.triggerNote = this.triggerNote.bind(this);
+  }
+  handleChange(e) {
+    e.preventDefault();
+    console.log('changing');
+    console.log(this);
+
+    // const synth = new Tone.Synth({
+    //   Oscillator: {
+    //     Type: this.
+    //   }
+    // })
+
+  }
+  // handleChangeForSawtooth(e) {
+  //   e.preventDefault();
+  //   const typeClone = Array.from(this.state.types);
+  //   const sawtooth = {
+  //     value: this.sawtoothChoice.value
+  //   }
+
+  //   typeClone.push(sawtooth)
+    
+  //   this.setState({
+  //     types: typeClone
+  //   })
+  // }
+  handleSubmitForSine(e) {
+    e.preventDefault();
+    const typeClone = Array.from(this.state.types);
+    const sine = {
+      value: this.sineChoice.value
     }
+
+    typeClone.push(sine)
+
+    this.setState({
+      type: '',
+      types: typeClone
+    })
+  }
+  handleSubmitForTriangle(e) {
+    e.preventDefault();
+    const typeClone = Array.from(this.state.types);
+    const triangle = {
+      value: this.triangleChoice.value
+    }
+
+    typeClone.push(triangle)
+
+    this.setState({
+      type: '',
+      types: typeClone
+    })
+  }
+  handleSubmitForSquare(e) {
+    e.preventDefault();
+    const typeClone = Array.from(this.state.types);
+    const square = {
+      value: this.squareChoice.value
+    }
+
+    typeClone.push(square)
+
+    this.setState({
+      type: '',
+      types: typeClone
+    })
+  }
+  handleSubmitForSawtooth(e) {
+    e.preventDefault();
+    const typeClone = Array.from(this.state.types);
+
+    const sawtooth = {
+      value: this.sawtoothChoice.value
+    }
+
+    typeClone.push(sawtooth)
+
+    this.setState({
+      type: '',
+      types: typeClone
+    })
+  }
+  triggerNote(e) {
+    e.preventDefault();
+    console.log(e)
+  }
+  render() {
+    return (
+      <div>
+        {/* <form action="" onSubmit={this.handleSubmit}>
+          <select name="oscillator" id="oscillatorChoice" onChange={this.handleChange}>
+            <option value="sawtooth" onChange={this.handleChangeForSawtooth}  ref={ref => this.sawtoothChoice = ref}>sawtooth</option>
+          </select>
+            <option value="sine" onChange={this.handleChange} ref={ref => this.sineChoice = ref}>sine</option>
+            <option value="triangle" onChange={this.handleChange} ref={ref => this.triangleChoice = ref}>triangle</option>
+            <option value="square" onChange={this.handleChange} ref={ref => this.squareChoice = ref}>square</option>
+          <input type="submit" value="Make a Synth!"/>
+        </form> */}
+        <form action="" onSubmit={this.handleSubmitForSawtooth}>
+          Sawtooth<input type="radio" name="oscillator" value="sawtooth" onChange={this.onChangeOscillator} ref={ref => this.sawtoothChoice = ref} />
+          <input type="submit" value="Make a Synth!" />
+        </form>
+        <form action="" onSubmit={this.handleSubmitForSine}>
+          Sine<input type="radio" name="oscillator" value="sine" onChange={this.onChangeOscillator} ref={ref => this.sineChoice = ref} />
+          <input type="submit" value="Make a Synth!" />
+        </form>
+        <form action="" onSubmit={this.handleSubmitForTriangle}>
+          Triangle<input type="radio" name="oscillator" value="triangle" onChange={this.onChangeOscillator} ref={ref => this.triangleChoice = ref} />
+          <input type="submit" value="Make a Synth!" />
+        </form>
+        <form action="" onSubmit={this.handleSubmitForSquare}>
+          Square<input type="radio" name="oscillator" value="square" onChange={this.onChangeOscillator} ref={ref => this.squareChoice = ref} />
+          <input type="submit" value="Make a Synth!" />
+        </form>
+        <section className="synths">
+          {this.state.types.map((type, i) => {
+            return (
+              <NewSynth 
+              key={i}
+              type={NewSynth.type}
+              triggerNote={this.triggerNote}/>
+            )
+          }
+
+          )}
+        </section>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
