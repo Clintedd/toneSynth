d5246a873f1064ee29f44aed29c57d3d05381ef0
Clint Lee <l.clint91@gmail.com>
2018-05-22 02:04:51 -0400

    border styling

--- dev/scripts/NewSynth.js
+++ dev/scripts/NewSynth.js
@@ -3,85 +3,89 @@ import React from 'react';
 export default function(props) {
     return (
         <div className="checkboxes">
-            <div>
-                <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
-            </div>
-            <div>
-                <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
-            </div>
-            <div>
-                <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
-            </div>
-            <div>
-                <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
-            </div>
-            <div>
-                <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
-            </div>
-            <div>
-                <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
-            </div>
-            <div>
-                <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
-            </div>
-            <div>
-                <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
-                <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
+            <div className="checkboxlayer1">
+                <div className="checkboxlayer2">
+                    <div>
+                        <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C5" onChange={() => props.triggerNoteC5(props.pickedType, props.firebaseKey)} />
+                    </div>
+                    <div>
+                        <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="D5" onChange={() => props.triggerNoteD5(props.pickedType, props.firebaseKey)} />
+                    </div>
+                    <div>
+                        <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="E5" onChange={() => props.triggerNoteE5(props.pickedType, props.firebaseKey)} />
+                    </div>
+                    <div>
+                        <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="F5" onChange={() => props.triggerNoteF5(props.pickedType, props.firebaseKey)} />
+                    </div>
+                    <div>
+                        <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="G5" onChange={() => props.triggerNoteG5(props.pickedType, props.firebaseKey)} />
+                    </div>
+                    <div>
+                        <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="A5" onChange={() => props.triggerNoteA5(props.pickedType, props.firebaseKey)} />
+                    </div>
+                    <div>
+                        <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="B5" onChange={() => props.triggerNoteB5(props.pickedType, props.firebaseKey)} />
+                    </div>
+                    <div>
+                        <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
+                        <input type="checkbox" id="C6" onChange={() => props.triggerNoteC6(props.pickedType, props.firebaseKey)} />
+                    </div>
+                </div>
             </div>
         </div>
     )
