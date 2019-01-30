b9a4f337ea3db89b9a6b5fbdd232e6938c32b12e
Clint Lee <l.clint91@gmail.com>
2019-01-30 11:56:46 -0500

    pushing work done

--- dev/scripts/NewSynth.js
+++ dev/scripts/NewSynth.js
@@ -1,97 +1,74 @@
 import React from 'react';
 
 const NewSynth = (props) => {
-    return (
-        <div className="checkboxes" id="sequencer">
-            <i className="fas fa-times" onClick={() => props.removeSynth(props.firebaseKey)}></i>
-            <div className="checkboxlayer1">
-                <div className="checkboxlayer2">
-                    <div>
-                        <label className="check-button check-button-c1" htmlFor="C51"><input type="checkbox" id="C51" onChange={() => props.triggerNote(props.pickedType, 'C5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c1" htmlFor="C52"><input type="checkbox" id="C52" onChange={() => props.triggerNote(props.pickedType, 'C5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c1" htmlFor="C53"><input type="checkbox" id="C53" onChange={() => props.triggerNote(props.pickedType, 'C5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c1" htmlFor="C54"><input type="checkbox" id="C54" onChange={() => props.triggerNote(props.pickedType, 'C5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c1" htmlFor="C55"><input type="checkbox" id="C55" onChange={() => props.triggerNote(props.pickedType, 'C5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c1" htmlFor="C56"><input type="checkbox" id="C56" onChange={() => props.triggerNote(props.pickedType, 'C5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c1" htmlFor="C57"><input type="checkbox" id="C57" onChange={() => props.triggerNote(props.pickedType, 'C5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c1" htmlFor="C58"><input type="checkbox" id="C58" onChange={() => props.triggerNote(props.pickedType, 'C5', props.pickedFilter)} /></label>
-                    </div>
-                    <div>
-                        <label className="check-button check-button-d1" htmlFor="D51"><input type="checkbox" id="D51" onChange={() => props.triggerNote(props.pickedType, 'D5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-d1" htmlFor="D52"><input type="checkbox" id="D52" onChange={() => props.triggerNote(props.pickedType, 'D5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-d1" htmlFor="D53"><input type="checkbox" id="D53" onChange={() => props.triggerNote(props.pickedType, 'D5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-d1" htmlFor="D54"><input type="checkbox" id="D54" onChange={() => props.triggerNote(props.pickedType, 'D5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-d1" htmlFor="D55"><input type="checkbox" id="D55" onChange={() => props.triggerNote(props.pickedType, 'D5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-d1" htmlFor="D56"><input type="checkbox" id="D56" onChange={() => props.triggerNote(props.pickedType, 'D5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-d1" htmlFor="D57"><input type="checkbox" id="D57" onChange={() => props.triggerNote(props.pickedType, 'D5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-d1" htmlFor="D58"><input type="checkbox" id="D58" onChange={() => props.triggerNote(props.pickedType, 'D5', props.pickedFilter)} /></label>
-                    </div>
-                    <div>
-                        <label className="check-button check-button-e1" htmlFor="E51"><input type="checkbox" id="E51" onChange={() => props.triggerNote(props.pickedType, 'E5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-e1" htmlFor="E52"><input type="checkbox" id="E52" onChange={() => props.triggerNote(props.pickedType, 'E5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-e1" htmlFor="E53"><input type="checkbox" id="E53" onChange={() => props.triggerNote(props.pickedType, 'E5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-e1" htmlFor="E54"><input type="checkbox" id="E54" onChange={() => props.triggerNote(props.pickedType, 'E5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-e1" htmlFor="E55"><input type="checkbox" id="E55" onChange={() => props.triggerNote(props.pickedType, 'E5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-e1" htmlFor="E56"><input type="checkbox" id="E56" onChange={() => props.triggerNote(props.pickedType, 'E5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-e1" htmlFor="E57"><input type="checkbox" id="E57" onChange={() => props.triggerNote(props.pickedType, 'E5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-e1" htmlFor="E58"><input type="checkbox" id="E58" onChange={() => props.triggerNote(props.pickedType, 'E5', props.pickedFilter)} /></label>
-                    </div>
-                    <div>
-                        <label className="check-button check-button-f1" htmlFor="F51"><input type="checkbox" id="F51" onChange={() => props.triggerNote(props.pickedType, 'F5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-f1" htmlFor="F52"><input type="checkbox" id="F52" onChange={() => props.triggerNote(props.pickedType, 'F5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-f1" htmlFor="F53"><input type="checkbox" id="F53" onChange={() => props.triggerNote(props.pickedType, 'F5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-f1" htmlFor="F54"><input type="checkbox" id="F54" onChange={() => props.triggerNote(props.pickedType, 'F5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-f1" htmlFor="F55"><input type="checkbox" id="F55" onChange={() => props.triggerNote(props.pickedType, 'F5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-f1" htmlFor="F56"><input type="checkbox" id="F56" onChange={() => props.triggerNote(props.pickedType, 'F5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-f1" htmlFor="F57"><input type="checkbox" id="F57" onChange={() => props.triggerNote(props.pickedType, 'F5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-f1" htmlFor="F58"><input type="checkbox" id="F58" onChange={() => props.triggerNote(props.pickedType, 'F5', props.pickedFilter)} /></label>
-                    </div>
-                    <div>
-                        <label className="check-button check-button-g1" htmlFor="G51"><input type="checkbox" id="G51" onChange={() => props.triggerNote(props.pickedType, 'G5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-g1" htmlFor="G52"><input type="checkbox" id="G52" onChange={() => props.triggerNote(props.pickedType, 'G5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-g1" htmlFor="G53"><input type="checkbox" id="G53" onChange={() => props.triggerNote(props.pickedType, 'G5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-g1" htmlFor="G54"><input type="checkbox" id="G54" onChange={() => props.triggerNote(props.pickedType, 'G5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-g1" htmlFor="G55"><input type="checkbox" id="G55" onChange={() => props.triggerNote(props.pickedType, 'G5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-g1" htmlFor="G56"><input type="checkbox" id="G56" onChange={() => props.triggerNote(props.pickedType, 'G5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-g1" htmlFor="G57"><input type="checkbox" id="G57" onChange={() => props.triggerNote(props.pickedType, 'G5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-g1" htmlFor="G58"><input type="checkbox" id="G58" onChange={() => props.triggerNote(props.pickedType, 'G5', props.pickedFilter)} /></label>
-                    </div>
-                    <div>
-                        <label className="check-button check-button-a1" htmlFor="A51"><input type="checkbox" id="A51" onChange={() => props.triggerNote(props.pickedType, 'A5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-a1" htmlFor="A52"><input type="checkbox" id="A52" onChange={() => props.triggerNote(props.pickedType, 'A5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-a1" htmlFor="A53"><input type="checkbox" id="A53" onChange={() => props.triggerNote(props.pickedType, 'A5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-a1" htmlFor="A54"><input type="checkbox" id="A54" onChange={() => props.triggerNote(props.pickedType, 'A5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-a1" htmlFor="A55"><input type="checkbox" id="A55" onChange={() => props.triggerNote(props.pickedType, 'A5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-a1" htmlFor="A56"><input type="checkbox" id="A56" onChange={() => props.triggerNote(props.pickedType, 'A5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-a1" htmlFor="A57"><input type="checkbox" id="A57" onChange={() => props.triggerNote(props.pickedType, 'A5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-a1" htmlFor="A58"><input type="checkbox" id="A58" onChange={() => props.triggerNote(props.pickedType, 'A5', props.pickedFilter)} /></label>
-                    </div>
-                    <div>
-                        <label className="check-button check-button-b1" htmlFor="B51"><input type="checkbox" id="B51" onChange={() => props.triggerNote(props.pickedType, 'B5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-b1" htmlFor="B52"><input type="checkbox" id="B52" onChange={() => props.triggerNote(props.pickedType, 'B5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-b1" htmlFor="B53"><input type="checkbox" id="B53" onChange={() => props.triggerNote(props.pickedType, 'B5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-b1" htmlFor="B54"><input type="checkbox" id="B54" onChange={() => props.triggerNote(props.pickedType, 'B5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-b1" htmlFor="B55"><input type="checkbox" id="B55" onChange={() => props.triggerNote(props.pickedType, 'B5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-b1" htmlFor="B56"><input type="checkbox" id="B56" onChange={() => props.triggerNote(props.pickedType, 'B5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-b1" htmlFor="B57"><input type="checkbox" id="B57" onChange={() => props.triggerNote(props.pickedType, 'B5', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-b1" htmlFor="B58"><input type="checkbox" id="B58" onChange={() => props.triggerNote(props.pickedType, 'B5', props.pickedFilter)} /></label>
-                    </div>
-                    <div>
-                        <label className="check-button check-button-c2" htmlFor="C61"><input type="checkbox" id="C61" onChange={() => props.triggerNote(props.pickedType, 'C6', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c2" htmlFor="C62"><input type="checkbox" id="C62" onChange={() => props.triggerNote(props.pickedType, 'C6', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c2" htmlFor="C63"><input type="checkbox" id="C63" onChange={() => props.triggerNote(props.pickedType, 'C6', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c2" htmlFor="C64"><input type="checkbox" id="C64" onChange={() => props.triggerNote(props.pickedType, 'C6', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c2" htmlFor="C65"><input type="checkbox" id="C65" onChange={() => props.triggerNote(props.pickedType, 'C6', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c2" htmlFor="C66"><input type="checkbox" id="C66" onChange={() => props.triggerNote(props.pickedType, 'C6', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c2" htmlFor="C67"><input type="checkbox" id="C67" onChange={() => props.triggerNote(props.pickedType, 'C6', props.pickedFilter)} /></label>
-                        <label className="check-button check-button-c2" htmlFor="C68"><input type="checkbox" id="C68" onChange={() => props.triggerNote(props.pickedType, 'C6', props.pickedFilter)} /></label>
-                    </div>
-                </div>
-                <input type="submit" value="PLAY" onClick={() => props.synthSequencer(props.pickedType, props.pickedFilter)} />
-                <input type="submit" className="stopButton" value="STOP" onClick={(e) => props.sequencerStop(e)} />
-            </div>
-        </div>
-    )
+		return (
+				<div className="checkboxes" id="sequencer">
+						<div className="synth-UI">
+								<div className="synth-info">
+										<p>Type: </p>
+										<p>{props.pickedType}</p>
+										<p>Filter: </p>
+										<p>{props.pickedFilter}</p>
+								</div>
+								<i className="fas fa-times closebutton" onClick={() => props.removeSynth(props.firebaseKey)}></i>
+						</div>
+						<div className="checkboxlayer1">
+								<div className="checkboxlayer2">
+										<div>
+												<label className="check-button check-button-wt" htmlFor="B41">
+														<input
+																type="checkbox"
+																id="B41"
+																onChange={() => props.triggerNote(props.pickedType, 'B4', props.pickedFilter)}
+														/>
+
+												</label>
+										</div>
+										<div>
+												<label className="check-button check-button-wt" htmlFor="C51"><input type="checkbox" id="C51" onChange={() => props.triggerNote(props.pickedType, 'C5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-bk" htmlFor="C#51"><input type="checkbox" id="C#51" onChange={() => props.triggerNote(props.pickedType, 'C#5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-wt" htmlFor="D51"><input type="checkbox" id="D51" onChange={() => props.triggerNote(props.pickedType, 'D5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-bk" htmlFor="D#51"><input type="checkbox" id="D#51" onChange={() => props.triggerNote(props.pickedType, 'D#5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-wt" htmlFor="E51"><input type="checkbox" id="E51" onChange={() => props.triggerNote(props.pickedType, 'E5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-wt" htmlFor="F51"><input type="checkbox" id="F51" onChange={() => props.triggerNote(props.pickedType, 'F5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-bk" htmlFor="F#51"><input type="checkbox" id="F#51" onChange={() => props.triggerNote(props.pickedType, 'F#5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-wt" htmlFor="G51"><input type="checkbox" id="G51" onChange={() => props.triggerNote(props.pickedType, 'G5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-bk" htmlFor="G#51"><input type="checkbox" id="G#51" onChange={() => props.triggerNote(props.pickedType, 'G#5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-wt" htmlFor="A51"><input type="checkbox" id="A51" onChange={() => props.triggerNote(props.pickedType, 'A5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-bk" htmlFor="A#51"><input type="checkbox" id="A#51" onChange={() => props.triggerNote(props.pickedType, 'A#5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-wt" htmlFor="B51"><input type="checkbox" id="B51" onChange={() => props.triggerNote(props.pickedType, 'B5', props.pickedFilter)} /></label>
+										</div>
+										<div>
+												<label className="check-button check-button-wt" htmlFor="C61"><input type="checkbox" id="C61" onChange={() => props.triggerNote(props.pickedType, 'C6', props.pickedFilter)} /></label>
+										</div>
+								</div>
+								{/* <input type="submit" value="PLAY" onClick={() => props.synthSequencer(props.pickedType, props.pickedFilter)} />
+								<input type="submit" className="stopButton" value="STOP" onClick={(e) => props.sequencerStop(e)} /> */}
+						</div>
+				</div>
+		)
 }
 
 export default NewSynth;
--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -30,12 +30,16 @@ class App extends React.Component {
     this.handleChangeType = this.handleChangeType.bind(this);
     this.handleSubmitType = this.handleSubmitType.bind(this);
     this.triggerNote = this.triggerNote.bind(this);
-    this.sequencerStop = this.sequencerStop.bind(this);
-    this.synthSequencer = this.synthSequencer.bind(this);
     this.removeSynth = this.removeSynth.bind(this);
+    // this.sequencerStop = this.sequencerStop.bind(this);
+    // this.synthSequencer = this.synthSequencer.bind(this);
 
   }
-  componentDidMount() {
+  componentDidMount(e) {
+    window.addEventListener('keydown', function (e) {
+      console.log(e.keyCode);
+    })
+
     const dbRef = firebase.database().ref('types');
     dbRef.on('value', (snapshot) => {
       const synthArray = [];
@@ -86,103 +90,114 @@ class App extends React.Component {
     console.log(dbRef);
   }
 
+
   triggerNote(pickedType, key, pickedFilter) {
     const typeChosen = pickedType
     const filterChosen = pickedFilter
+    
+
     const synth = new Tone.MonoSynth({ 
       oscillator: { 
         type: typeChosen 
       },
       filter: {
         type: filterChosen
-      } 
+      },
+      envelope: {
+        attack: 0.005,
+        decay: 0.003
+      }
     })
+
     synth.triggerAttackRelease(key, '32n').toMaster()
-
   }
-  sequencerStop(e) {
-    e.preventDefault();
-    console.log('hi');
-    // Tone.Transport.unsync();
-    Tone.Transport.stop();
-  }
-  synthSequencer(pickedType, pickedFilter) {
-    const sequencerType = pickedType
-    const sequencerFilter = pickedFilter
 
-    const synths = [
-      new Tone.MonoSynth(),
-      new Tone.MonoSynth(),
-      new Tone.MonoSynth(),
-      new Tone.MonoSynth(),
-      new Tone.MonoSynth(),
-      new Tone.MonoSynth(),
-      new Tone.MonoSynth(),
-      new Tone.MonoSynth()
-    ]
+  // addDistortion() {
+  //   var distortion = new Tone.Distortion(0.4).toMaster();
+  //   synth.connect(distortion);
+  // }
 
-    synths[0].oscillator.type = sequencerType;
-    synths[1].oscillator.type = sequencerType;
-    synths[2].oscillator.type = sequencerType;
-    synths[3].oscillator.type = sequencerType;
-    synths[4].oscillator.type = sequencerType;
-    synths[5].oscillator.type = sequencerType;
-    synths[6].oscillator.type = sequencerType;
-    synths[7].oscillator.type = sequencerType;
+  // sequencerStop(e) {
+  //   e.preventDefault();
+  //   Tone.Transport.stop();
+  // }
+  // synthSequencer(pickedType, pickedFilter) {
+  //   const sequencerType = pickedType
+  //   const sequencerFilter = pickedFilter
 
-    synths[0].filter.type = sequencerFilter;
-    synths[1].filter.type = sequencerFilter;
-    synths[2].filter.type = sequencerFilter;
-    synths[3].filter.type = sequencerFilter;
-    synths[4].filter.type = sequencerFilter;
-    synths[5].filter.type = sequencerFilter;
-    synths[6].filter.type = sequencerFilter;
-    synths[7].filter.type = sequencerFilter;
+  //   const synths = [
+  //     new Tone.MonoSynth(),
+  //     new Tone.MonoSynth(),
+  //     new Tone.MonoSynth(),
+  //     new Tone.MonoSynth(),
+  //     new Tone.MonoSynth(),
+  //     new Tone.MonoSynth(),
+  //     new Tone.MonoSynth(),
+  //     new Tone.MonoSynth()
+  //   ]
+
+  //   synths[0].oscillator.type = sequencerType;
+  //   synths[1].oscillator.type = sequencerType;
+  //   synths[2].oscillator.type = sequencerType;
+  //   synths[3].oscillator.type = sequencerType;
+  //   synths[4].oscillator.type = sequencerType;
+  //   synths[5].oscillator.type = sequencerType;
+  //   synths[6].oscillator.type = sequencerType;
+  //   synths[7].oscillator.type = sequencerType;
+
+  //   synths[0].filter.type = sequencerFilter;
+  //   synths[1].filter.type = sequencerFilter;
+  //   synths[2].filter.type = sequencerFilter;
+  //   synths[3].filter.type = sequencerFilter;
+  //   synths[4].filter.type = sequencerFilter;
+  //   synths[5].filter.type = sequencerFilter;
+  //   synths[6].filter.type = sequencerFilter;
+  //   synths[7].filter.type = sequencerFilter;
     
 
-    synths.forEach(synth => synth.toMaster());
+  //   synths.forEach(synth => synth.toMaster());
 
-    const container = document.querySelector("#sequencer");
+  //   const container = document.querySelector("#sequencer");
 
-    const rows = container.querySelectorAll('div.checkboxlayer2 > div'),
-          notes = ['C5','D5','E5','F5','G5','A5','B5','C6']
-    let index = 0;
+  //   const rows = container.querySelectorAll('div.checkboxlayer2 > div'),
+  //         notes = ['C5','D5','E5','F5','G5','A5','B5','C6']
+  //   let index = 0;
 
-    Tone.Transport.scheduleRepeat(repeat, '8n');
-    Tone.Transport.start();
+  //   Tone.Transport.scheduleRepeat(repeat, '8n');
+  //   Tone.Transport.start();
 
-    function repeat(time) {
-      let step = index % 8;
-      for (let i = 0; i < rows.length; i++) {
-        let synth = synths[i],
-            note = notes[i],
-            row = rows[i],
-            input = row.querySelector(`input:nth-child(${step + 1})`);
-        if (input.checked)
-        synth.triggerAttackRelease(note, '8n', time);
-      }
-      index++;
-      Tone.currentTime = 0;
-    }
-  }
+  //   function repeat(time) {
+  //     let step = index % 8;
+  //     for (let i = 0; i < rows.length; i++) {
+  //       let synth = synths[i],
+  //           note = notes[i],
+  //           row = rows[i],
+  //           input = row.querySelector(`input:nth-child(${step + 1})`);
+  //       if (input.checked)
+  //       synth.triggerAttackRelease(note, '8n', time);
+  //     }
+  //     index++;
+  //     Tone.currentTime = 0;
+  //   }
+  // }
 
   removeSynth(keyToRemove) {
     const dbRef = firebase.database().ref(`types/${keyToRemove}`)
     dbRef.remove();
-
   }
 
   render() {
     return (
-      <div className="main">
-        <div className="titleWrapper">
-          <h1 className="title">SYNTHMAKER</h1>
-        </div>
-        <div className="wrapper">
-          <div className="forms">
-            <div>
+      <body>
+        <header className="main">
+          <div className="titleWrapper">
+            <h1 className="title">SYNTHMAKER</h1>
+          </div>
+          <div className="wrapper">
+            <div className="forms">
               <form action="" onSubmit={this.handleSubmitType}>
                 <div className="select-style">
+                  <h5 className="select-title">Type</h5>
                   <select name="types" id="" ref={ref => this.typeChoice = ref} onChange={this.handleChangeType}>
                     <option value="sawtooth" >Sawtooth</option>
                     <option value="sine" >Sine</option>
@@ -191,6 +206,7 @@ class App extends React.Component {
                   </select>
                 </div>
                 <div className="select-style">
+                  <h5 className="select-title">Filter</h5>
                   <select name="filters" id="" ref={ref => this.filterChoice = ref} onChange={this.handleChangeFilter}>
                     <option value="lowpass">Lowpass</option>
                     <option value="highpass">Highpass</option>
@@ -205,26 +221,26 @@ class App extends React.Component {
                 <input type="submit" value="Play Synth!"/>
               </form>
             </div>
+            <section>
+              {this.state.types.map((typePicked) => {
+                return <NewSynth
+                  key={typePicked.key}
+                  pickedFilter={typePicked.filter}
+                  pickedType={typePicked.value}
+                  triggerNote={this.triggerNote}
+                  synthSequencer={this.synthSequencer}
+                  sequencerStop={this.sequencerStop}
+                  removeSynth={this.removeSynth}
+                  firebaseKey={typePicked.key} />
+                }
+              )}
+            </section>
           </div>
-          <section className="synths">
-            {this.state.types.map((typePicked) => {
-              return <NewSynth
-                key={typePicked.key}
-                pickedFilter={typePicked.filter}
-                pickedType={typePicked.value}
-                triggerNote={this.triggerNote}
-                synthSequencer={this.synthSequencer}
-                sequencerStop={this.sequencerStop}
-                removeSynth={this.removeSynth}
-                firebaseKey={typePicked.key} />
-              }
-            )}
-          </section>
-          {/* <footer>
-            <h5>CLINT LEE ©</h5>
-          </footer> */}
-        </div>
-      </div>
+        </header>
+        <footer>
+          © Made by Clint Lee
+        </footer>
+      </body>
     )
   }
 }
